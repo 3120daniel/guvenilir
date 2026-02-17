1b11a377a1ebbe95e1e6a4237f299779bd07096c
Osorachukwu <oezewi@gmail.com>
2026-02-17 18:57:32 +0100

    two cards

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -20,7 +20,7 @@ export default function OurServices() {
     return (
         <div>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-                <div className="grid gap-10 lg:grid-cols-2">
+                <div className="grid lg:gap-10 lg:grid-cols-2">
 
                     {/*  */}
                     <div className="lg:px-10">
@@ -32,12 +32,21 @@ export default function OurServices() {
                         </p>
 
                     </div>
-                    <div className='w-full'>
-                        <img
-                            className="object-contain h-full w-full rounded sm:h-96"
-                            src={atmCard2}
-                            alt=""
-                        />
+                    <div>
+                        <div className='w-full'>
+                            <img
+                                className="object-contain h-full w-full rounded sm:h-96"
+                                src={atmCard}
+                                alt=""
+                            />
+                        </div>
+                        <div className='w-full'>
+                            <img
+                                className="object-contain h-full w-full rounded sm:h-96"
+                                src={atmCard2}
+                                alt=""
+                            />
+                        </div>
                     </div>
 
                 </div>
